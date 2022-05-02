964a611d6c5f60a592d4b4a79dcd533dea45bd78
Luis Pablo Ortiz Barrundia <88795534+lortiz2016328@users.noreply.github.com>
2022-05-02 16:45:10 -0600

    Agregando ProductosEmpresa

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -5,7 +5,7 @@ const md_autenticacion =  require('../middlewares/autenticacion');
 const api = express.Router()
 
 api.get('/productos', productosController.obtenerProductos);
-api.post('/productos/agregar', productosController.agregarProducto);
+api.post('/productos/agregar', md_autenticacion.Auth,  productosController.agregarProducto);
 api.put('/editarProducto/:idProducto',md_autenticacion.Auth, productosController.editarProducto);
 api.delete('/eliminarProducto/:idProducto', md_autenticacion.Auth,productosController.eliminarProducto);
  
--- src/routes/sucursales.routes.js
+++ src/routes/sucursales.routes.js
@@ -5,7 +5,7 @@ const md_autenticacion = require('../middlewares/autenticacion');
 const api = express.Router()
 
 api.get('/sucursales', sucursalesController.obtenerSucursales);
-api.post('/sucursales/agregar', sucursalesController.agregarSucursal);
+api.post('/sucursales/agregar', md_autenticacion.Auth, sucursalesController.agregarSucursal);
 api.put('/editarSucursal/:idSucursal', md_autenticacion.Auth, sucursalesController.editarSucursal);
 api.delete('/eliminarSucursal/:idSucursal', md_autenticacion.Auth, sucursalesController.eliminarSucursal);
 

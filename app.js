61da0e6135b7fd3068e3634029e0f142f6149ce2
Luis Pablo Ortiz Barrundia <88795534+lortiz2016328@users.noreply.github.com>
2022-05-18 15:44:20 -0600

    Agregando Productos Sucursales

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var app = express();
 const EmpresasRutas = require('./src/routes/empresas.routes');
 const ProductosRutas = require('./src/routes/productos.routes');
 const SucursalesRutas = require('./src/routes/sucursales.routes');
+const ProductosSucursalesRutas = require('./src/routes/productosSucursales.routes');
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/api', EmpresasRutas, ProductosRutas, SucursalesRutas);
+app.use('/api', EmpresasRutas, ProductosRutas, SucursalesRutas, ProductosSucursalesRutas);
 
 module.exports = app;

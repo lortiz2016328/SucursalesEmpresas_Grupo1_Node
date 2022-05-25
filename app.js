60c95f38423c784c34ca42d989aea6727f32346c
Luis Pablo Ortiz Barrundia <88795534+lortiz2016328@users.noreply.github.com>
2022-05-24 22:50:41 -0600

    Cambios Finales

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const cors = require('cors');
 var app = express();
 
-const EmpresasRutas = require('./src/routes/empresas.routes');
+const EmpresaRutas = require('./src/routes/empresas.routes');
 const ProductosRutas = require('./src/routes/productos.routes');
 const SucursalesRutas = require('./src/routes/sucursales.routes');
 const ProductosSucursalesRutas = require('./src/routes/productosSucursales.routes');
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cors());
+app.use('/api', EmpresaRutas, ProductosRutas, SucursalesRutas, ProductosSucursalesRutas);
 
-app.use('/api', EmpresasRutas, ProductosRutas, SucursalesRutas, ProductosSucursalesRutas);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-const usuarioControlador = require('./src/controllers/empresas.controller');
+const Empresa = require('./src/models/empresas.model');
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('./src/services/jwt');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/SucursalesEmpresas', { useNewUrlParser: true, useUnifiedTopology: true}).then(() =>{
-    console.log("Se encuentra dentro a la base de datos");
-    app.listen(3000, function() {
-        console.log("hola mundo, estoy en el puerto 3000!")
-    })
-}).catch(error => console.log(error))
+mongoose.connect('mongodb://localhost:27017/SucursalesEmpresas', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
 
-usuarioControlador.usuarioInicial()
+    app.listen(3000, function () {
+        console.log("Hola Mundo!");
+        console.log("La base de datos esta corriendo en el puerto 3000!");
+        Empresa.find({ nombre: 'SuperAdmin' }, (err, usuarioEcontrado) => {
+            if (usuarioEcontrado == 0) {
+
+                bcrypt.hash('123456', null, null, (err, passwordEncriptada) => {
+                    Empresa.create({
+                        nombre: 'SuperAdmin',
+                        email: 'admin@kinal.com',
+                        rol: 'SuperAdmin',
+                        password: passwordEncriptada
+                        
+                    })
+
+                });
+            } else {
+
+            }
+
+        })
+    })
+
+
+}).catch(error => console.log(error))
